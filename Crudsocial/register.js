e40addcdae398c1794fdd12f05f51630a1307a64
JuanDavidBarr <barrera.juanda7@gmail.com>
2025-06-28 12:09:35 -0500

    Feat: validaciones en contraseñas login

--- Crudsocial/register.js
+++ Crudsocial/register.js
@@ -4,16 +4,34 @@ const myForm = document.getElementById("myForm");
 const myInputConfirmPasswordContainer = document.getElementById("confirmPasswordContainer");
 //array of form elements without the button
 const myInput = myForm.elements;
-
-
+const errorMessage = document.createElement("span");
+//remove error message and error styles if any
+myForm.elements["password"].addEventListener("click", ()=>{
+    if (errorMessage){
+        errorMessage.remove();
+        myForm.elements["password"].style.borderColor = "black";
+        myForm.elements["passwordConfirmation"].style.borderColor = "black";
+    }
+})
 //Capture the information on the input field
 myForm.addEventListener("submit", (event)=>{
     //Prevent the page to reload when clicking on the submit button
     event.preventDefault();
+    //Capture input from password and confirm password fields
     const password = myForm.elements["password"].value;
     const passwordConfirm = myForm.elements["passwordConfirmation"].value;
+    //Validate password and confirm password are the same
     if (password !== passwordConfirm){
-        myInputConfirmPasswordContainer.innerHTML
+        //styling error message and input fields
+        errorMessage.style.color = "red";
+        errorMessage.style.fontSize = "x-small";
+        errorMessage.style.display = "flex";
+        myForm.elements["password"].style.borderColor = "red";
+        myForm.elements["passwordConfirmation"].style.borderColor = "red";
+        errorMessage.innerHTML = "Paswords do not match";
+        //add span with alert message below confirmpassowrd field
+        myInputConfirmPasswordContainer.appendChild(errorMessage);
+        return;
     }
     //loop over the array of inputs
     for (const element of myInput) {
@@ -23,8 +41,9 @@ myForm.addEventListener("submit", (event)=>{
             localStorage.setItem(element.name, element.value);
         }
     }
-
 })
+
+
 
 
 
