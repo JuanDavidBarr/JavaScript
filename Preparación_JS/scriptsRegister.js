45a6d6e59c677af5d233c4c3e36aaf167e091d7e
JuanDavidBarr <133900096+JuanDavidBarr@users.noreply.github.com>
2025-07-10 15:31:16 -0500

    feat: registro lógica

--- Preparación_JS/scriptsRegister.js
+++ Preparación_JS/scriptsRegister.js
@@ -16,14 +16,14 @@ async function getUsers(url) {
     }
 }
 //POST METHOD
-async function createUsers(url, data) {
+async function createUsers(url,data1) {
     try{
         const response = await fetch(url+"Users",{
             method : "POST",
             headers : {
                 "Content-Type" : "application/json"
             },
-            body : JSON.stringify(data)
+            body : JSON.stringify(data1)
         });
         if(!response.ok){
             throw new Error("Couldn't add resource");
@@ -38,7 +38,9 @@ async function createUsers(url, data) {
 registerButton.addEventListener("click", async (e)=>{
     e.preventDefault();
     const data = await getUsers(URL_APP);
-    let isEmpty; 
+    let isEmpty;
+    let isNotAvailable;
+    //VALIDATING INPUTS ARE NOT EMPTY
     registerInputContent.forEach(element => {
         if(element.value.length === 0){
             const emptyMessage = document.querySelector(`.${element.name}`);
@@ -49,8 +51,37 @@ registerButton.addEventListener("click", async (e)=>{
     if (isEmpty){
         return;
     }
-    const username = registerInputContent[0].value;
-    const email = registerInputContent[1].value;
+    const username = registerInputContent[0].value.trim();
+    const email = registerInputContent[1].value.trim();
     const password = registerInputContent[2].value;
     const confirmpassword = registerInputContent[3].value;
+    //VALIDATING THERE IS NO USERNAME OR EMAIL REPEATED
+    data.forEach(element => {
+        if (username === element.name){
+            const unavailableMessage = document.querySelector(".username-unavailable");
+            unavailableMessage.classList.remove("is-hidden");
+            isNotAvailable = true;
+            return;
+        } else if (email === element.email){
+            const unavailableMessage = document.querySelector(".email-unavailable");
+            unavailableMessage.classList.remove("is-hidden");
+            isNotAvailable = true;
+            return;
+        }
+    });
+    if (isNotAvailable){
+        return;
+    }
+    //VALIDATING PASSWORD AND PASSWORDCONFIRMATION
+    if(!(password === confirmpassword)){
+        const noMatchMessage = document.querySelector(".no-match");
+        noMatchMessage.classList.remove("is-hidden");
+        return;
+    }
+    const newUser = {
+        "name": username,
+        "email": email,
+        "password": password
+    }
+    createUsers(URL_APP,newUser);
 })
