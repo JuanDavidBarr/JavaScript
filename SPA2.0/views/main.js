26f9a69203dc4e0ea34fab605c1ba6e4972a4fa3
JuanDavidBarr <barrera.juanda7@gmail.com>
2025-07-12 14:16:10 -0500

    feat: SPA

---
+++ SPA2.0/views/main.js
@@ -0,0 +1,11 @@
+// scripts/main.js
+import { renderRoute } from '../router.js';
+
+const app = document.getElementById('container');
+
+// Obtiene la ruta actual y la etiqueta donde se le va inserar el contenido
+const handleRoute = () => renderRoute(location.hash, app);
+// Cuando cambia el hash ejecuta la función
+window.addEventListener('hashchange', handleRoute);
+// Cuando se carga la página ejecuta la funcion
+window.addEventListener('DOMContentLoaded', handleRoute);
