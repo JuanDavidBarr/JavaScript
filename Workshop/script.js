834d57640b1309316c4e3af6b344f6116faf1366
JuanDavidBarr <133900096+JuanDavidBarr@users.noreply.github.com>
2025-07-11 21:36:39 -0500

    feat: workshop prueba

---
+++ Workshop/script.js
@@ -0,0 +1,241 @@
+const anclas = document.querySelectorAll("a");
+const container = document.querySelector(".container");
+
+// anclas.forEach(element => {
+//     element.addEventListener("click",(e) =>{
+//         e.preventDefault();
+//         render(element.textContent);
+             
+//     })
+// });
+
+// function render (content){
+//     container.innerHTML = `<h1>${content}</h1>`
+// }
+//--------------------------------------------------------------------------------------------------------------------------------------------------
+let router = {
+    "/home" :homeRender,
+    "/contact" :contactRender,
+    "/terms" :termsRender,
+    "/users" :usersRender
+}
+
+function homeRender (){
+    container.innerHTML = "<h1>Home</h1>";
+    window.location.hash = "#/home";
+}
+
+function contactRender (){
+    container.innerHTML = "<h1>Contact</h1>";
+    window.location.hash = "#/contact";
+}
+
+function termsRender (){
+    container.innerHTML = "<h1>Terms</h1>";
+    window.location.hash = "#/terms";
+}
+
+function usersRender (){
+    container.innerHTML = `<table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Add
+                        </button>
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Add user</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="mb-3 row">
+                                            <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                                            <div class="col-sm-10">
+                                              <input type="text" class="form-control" id="inputName">
+                                            </div>
+                                        </div>
+                                          <div class="mb-3 row">
+                                            <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                                            <div class="col-sm-10">
+                                              <input type="email" class="form-control" id="inputEmail">
+                                            </div>
+                                          </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>
+                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Delete
+                        </button>
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Delete user</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="mb-3 row">
+                                            <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                                            <div class="col-sm-10">
+                                              <input type="text" class="form-control" id="inputName">
+                                            </div>
+                                        </div>
+                                          <div class="mb-3 row">
+                                            <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                                            <div class="col-sm-10">
+                                              <input type="email" class="form-control" id="inputEmail">
+                                            </div>
+                                          </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Update
+                        </button>
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modify user</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="mb-3 row">
+                                            <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                                            <div class="col-sm-10">
+                                              <input type="text" class="form-control" id="inputName">
+                                            </div>
+                                        </div>
+                                          <div class="mb-3 row">
+                                            <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                                            <div class="col-sm-10">
+                                              <input type="password" class="form-control" id="inputEmail">
+                                            </div>
+                                          </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>`
+    async function getInfo (){
+        const response = await fetch("http://localhost:3000/users")
+        const data = await response.json();
+        data.forEach(element =>{
+            //inyectar al tbody no al container
+            container.innerHTML += `<th scope="row">1</th>
+                    <td>${element.name}</td>
+                    <td>${element.email}</td>
+                    <td>
+                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Delete
+                        </button>
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Delete user</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="mb-3 row">
+                                            <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                                            <div class="col-sm-10">
+                                              <input type="text" class="form-control" id="inputName">
+                                            </div>
+                                        </div>
+                                          <div class="mb-3 row">
+                                            <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                                            <div class="col-sm-10">
+                                              <input type="email" class="form-control" id="inputEmail">
+                                            </div>
+                                          </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Update
+                        </button>
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modify user</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="mb-3 row">
+                                            <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                                            <div class="col-sm-10">
+                                              <input type="text" class="form-control" id="inputName">
+                                            </div>
+                                        </div>
+                                          <div class="mb-3 row">
+                                            <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                                            <div class="col-sm-10">
+                                              <input type="password" class="form-control" id="inputEmail">
+                                            </div>
+                                          </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </td>`
+        })
+    }
+    getInfo();
+    window.location.hash = "#/users";
+}
+anclas.forEach(element => {
+    element.addEventListener("click",(e) =>{
+        e.preventDefault();
+        let path = e.target.getAttribute("href");
+        router[path]();           
+    })
+});
+
+
+
+// let path = e.target.getAttribute("href");
+//     router[path]();
+
+
