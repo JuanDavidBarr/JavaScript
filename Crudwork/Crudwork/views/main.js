115deed46181131effd3788b74dc4b8e2256edd4
JuanDavidBarr <133900096+JuanDavidBarr@users.noreply.github.com>
2025-07-16 21:06:16 -0500

    register

---
+++ Crudwork/Crudwork/views/main.js
@@ -0,0 +1,10 @@
+// import {renderRoute} from '../router.js';
+
+// const app = document.getElementById("app");
+
+// const handleRoute = () => renderRoute(location.hash, app);
+
+// window.addEventListener("hashchange", handleRoute)
+
+// window.addEventListener("DOMContentLoaded", handleRoute);
+
